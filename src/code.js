bee38b4088e6c143dd321d3f7a1904dfd628040b
Francisco Luis Campo <fcampo7@hotmail.com>
2023-09-20 18:50:21 -0300

    Refactorización y documentación

--- src/code.js
+++ src/code.js
@@ -1,11 +1,21 @@
+/**
+ * Matriz que representa el sistema a percolar.
+ * @type {number[][]}
+ */
 var matriz=[];
 
+/**
+ * Genera una matriz con un tamaño y una probabilidad especificada para la percolación.
+ */
 function generarMatriz(){
+	const buttonPercolar=document.querySelector('#btnPercolar');
 	let inputtam = document.querySelector('#my-input');
 	let slider = document.querySelector('#my-slider');
+
 	const tam=inputtam.value;
 	const probabilidad=slider.value;
 	
+	// Crea la matriz a percolar
 	for(let i=0; i<tam; i++){
 		matriz[i]=[];
 		for(let j=0; j<tam; j++){
@@ -15,31 +25,74 @@ function generarMatriz(){
 			else matriz[i][j]=1;
 		}
 	}
-	percolar();
-	drawSquare();
+	drawMatrix();
+	buttonPercolar.disabled = false;
 }
 
-function hide(opc){
-	let ns=document.querySelector('#snackbarS');
-	let nf=document.querySelector('#snackbarF');
-	if(opc=='snackbarS'){
-		ns.className=ns.className.replace("show", "");
+/**
+ * Crea y muestra un Snackbar con el resultado de la percolación.
+ * @param {boolean} flag - Indica si hay percolación o no.
+ */
+function createSnackbars(flag) {
+	let snackBar=document.createElement('div');
+	let buttonDelete=document.createElement('button');
+	buttonDelete.className="delete";
+	buttonDelete.onclick=function(){
+		snackBar.className=snackBar.className.replace("snackbar","");
+		document.body.removeChild(snackBar); 
+		snackBar.removeChild(buttonDelete);
+	};
+	snackBar.appendChild(buttonDelete);
+	
+	if(flag){
+		snackBar.className="snackbar notification is-success";
+		snackBar.appendChild(document.createTextNode("Hay percolación"));
 	}
 	else{
-		nf.className=ns.className.replace("show", "");
+		snackBar.className="snackbar notification is-danger";
+		snackBar.appendChild(document.createTextNode("No hay percolación"));
 	}
+
+	// Añade el snackbar al body
+	document.body.appendChild(snackBar);
+
+	// Remueve el snackbar después de 3 segundos
+	setTimeout(
+		function(){
+			snackBar.className=snackBar.className.replace("snackbar","");
+			document.body.removeChild(snackBar); 
+			snackBar.removeChild(buttonDelete);
+		}, 3000
+	);
 }
 
+/**
+ * Realiza la percolación en la matriz y muestra el resultado en un Snackbar.
+ */
 function percolar(){
+
+	// Desactiva el botón Percolar
+	const buttonPercolar=document.querySelector('#btnPercolar');
+	buttonPercolar.disabled = true;
+	
+	/**
+	 * Realiza el algoritmo de percolación en la matriz, iniciando desde la posición dada.
+	 * @param {number} posx - Posición en el eje x.
+	 * @param {number} posy - Posición en el eje y.
+	 */
 	function __percolar(posx, posy){
-		// Condiciones de salida (Que los valores de "x" e "y" excedan el tama?o de la matriz o que la matriz en la posici?n dada sea != 0)
+		// Condiciones de salida (Que los valores de "x" e "y" excedan el tamaóo de la matriz o que la matriz en la posición dada sea != 0)
 		if (posx < 0 || posy < 0 || posx >= matriz.length || posy >= matriz[0].length || matriz[posx][posy] != 0)
 			return;
-
-		// Asignamos un valor 2 en la posici?n "x" e "y" de la matriz que sera representado como agua
+	
+		// Asigna un valor 2 en la posición "x" e "y" de la matriz que sera representado como agua
 		matriz[posx][posy] = 2;
-
-		// Pasamos a las posiciones adyacentes
+		
+		// Dibuja el cuadrado en la posición "x" e "y"
+		// No interviene en el algoritmo de percolación
+		drawSquareAqua(posx, posy);
+		
+		// Pasa a las posiciones adyacentes
 		__percolar(posx, posy + 1);
 		__percolar(posx + 1, posy);
 		__percolar(posx - 1, posy);
@@ -51,40 +104,36 @@ function percolar(){
 			__percolar(i, 0);
 		}
 	}
-
+	
 	var flag=false;
-
+	
 	for(let i=0; i<matriz.length; i++){
 		if (matriz[i][matriz.length - 1] == 2){
 			flag=true;
 		}
 	}
 
-	if(flag){
-		let ns=document.querySelector('#snackbarS');
-		ns.className="show snackbar notification is-success";
-		setTimeout(function(){ ns.className = ns.className.replace("show", ""); }, 3000);
-	}
-	else{
-		let nf=document.querySelector('#snackbarF');
-		nf.className="show snackbar notification is-danger";
-		setTimeout(function(){ nf.className = nf.className.replace("show", ""); }, 3000);
-	}
-	return flag;
+	createSnackbars(flag);
+
 }
 
-//activar botón generar
+/**
+ * Activa el botón de generar según la entrada del usuario.
+ */
 function activateButton(){
 	let inputtam = document.querySelector('#my-input');
-	let button=document.querySelector('#btn');
+	let button=document.querySelector('#btnGenerarMatriz');
 	
 	const tam=inputtam.value;
+
 	button.disabled = tam === '';
 }
 
 
-//Dibujar matriz
-function drawSquare() {
+/**
+ * Dibuja la matriz en un canvas.
+ */
+function drawMatrix() {
 	let canvas = document.querySelector('#my-canvas');
 	let context = canvas.getContext('2d');
 	let inputtam = document.querySelector('#my-input');
@@ -108,12 +157,30 @@ function drawSquare() {
 				context.rect(i*side, j*side, side, side);
 				context.fillRect(i*side+1, j*side+1, side-1, side-1);
 			}
-			else if(matriz[i][j]==2){
-				context.fillStyle="aqua";
-				context.rect(i*side, j*side, side, side);
-				context.fillRect(i*side+1, j*side+1, side-1, side-1);
-			}
 			context.stroke();
 		}
 	}
+}
+
+/**
+ * Dibuja un cuadrado azul en una posición dada.
+ * @param {number} posx - Posición en el eje x.
+ * @param {number} posy - Posición en el eje y.
+ */
+function drawSquareAqua(posx, posy){
+	let canvas = document.querySelector('#my-canvas');
+	let context = canvas.getContext('2d');
+	let inputtam = document.querySelector('#my-input');
+
+	const tam=inputtam.value;
+
+	const side=Math.min(canvas.height,canvas.width)/tam;
+
+	context.beginPath();
+	
+	context.fillStyle="aqua";
+	context.rect(posx*side, posy*side, side, side);
+	context.fillRect(posx*side+1, posy*side+1, side-1, side-1);
+	
+	context.stroke();
 }
